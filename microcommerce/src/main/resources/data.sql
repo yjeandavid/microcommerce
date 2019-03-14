64b2b8fa40b3073f6e6b72a498fcc6df5b643124
Claude-Clément Yapo <ccyapo@kezber.com>
2019-03-14 10:01:38 -0400

    Persister la BD

--- microcommerce/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO product VALUES(1, 'Ordinateur Portable', 350, 120);
-INSERT INTO product VALUES(2, 'Aspirateur Robot', 500, 200);
-INSERT INTO product VALUES(3, 'Table de ping pong', 750, 400);
